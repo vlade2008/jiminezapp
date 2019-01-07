cc370bc3e7df3d8fd21c1cb7e76da51e5fc6290b
Christian John Saclao <vlade1356@gmail.com>
2019-01-07 10:45:29 +0800

    for deployting

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ var cors = require('cors');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -25,7 +26,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -33,6 +34,11 @@ app.use('/medicines', medicines);
 app.use('/patients', patients);
 app.use('/orders', orders);
 app.use('/templates',templates);
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
