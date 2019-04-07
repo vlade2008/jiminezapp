705e392708be09c7633db9954b0b53e5f9927321
Christian John Saclao <vlade1356@gmail.com>
2019-04-07 16:08:53 +0800

    update

---
+++ frontend/build/precache-manifest.e1a415e7a024a09551174b1e851a4c18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "01e0a9e7d11f379bb5601d99a74331fb",
+    "url": "/static/media/print.01e0a9e7.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8cb188a8ff594d60641b",
+    "url": "/static/js/main.8cb188a8.chunk.js"
+  },
+  {
+    "revision": "bc91ec72cb5f3f92afa4",
+    "url": "/static/js/1.bc91ec72.chunk.js"
+  },
+  {
+    "revision": "8cb188a8ff594d60641b",
+    "url": "/static/css/main.0efca89c.chunk.css"
+  },
+  {
+    "revision": "bc91ec72cb5f3f92afa4",
+    "url": "/static/css/1.a39cd8e6.chunk.css"
+  },
+  {
+    "revision": "17066f217584cb8cac3bee91d0918cfb",
+    "url": "/index.html"
+  }
+];
